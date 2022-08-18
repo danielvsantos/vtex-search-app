4757f6644d4cb650d5b10549b421e0ba8cd1868b
daniel.viana <daniel.viana@vtex.com.br>
2022-08-18 12:19:32 +0200

    Initial Upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require ("express");
+const bodyParser = require("body-parser");
+const request = require('request');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res) => {
+    var searchTerm = req.body.search;
+    var urlWithTerm = "https://groceriesspain.vtexcommercestable.com.br/api/catalog_system/pub/products/search/" + searchTerm; 
+    var options = {
+        method: 'GET',
+        url: urlWithTerm,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      };
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.send(body);
+        console.log(body);
+      });
+
+
+})
+
+app.listen(3000, () => {
+    console.log(`Server up and running on port ${port}`);
+})
+
+
+
